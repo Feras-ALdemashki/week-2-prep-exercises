2bdc7fcec46c0bca7aed05f96dc5564db7cdefd7
feras <feras.aldemashki@hotmail.com>
2024-11-23 01:08:58 +0100

    2-traffic-light

---
+++ traffic-light-2.js
@@ -0,0 +1,20 @@
+const trafficLight = {
+    possibleStates: ["green", "orange", "red"],
+    stateIndex: 0,
+  };
+  
+  let cycle = 0;
+  
+  while (cycle < 2) {
+    const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+    console.log("The traffic light is on", currentState);
+  
+    if (currentState === "green") {
+      trafficLight.stateIndex = 1; 
+    } else if (currentState === "orange") {
+      trafficLight.stateIndex = 2; 
+    } else {
+      trafficLight.stateIndex = 0; 
+      cycle++; 
+    }
+  }
